05b8230bb4abdbfbff06629f43e5a658c44e2e5b
Marc Terradas <terradas.marc@gmail.com>
2023-06-18 15:06:36 +0200

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// this file is a configuration file for configuring vitest library for testing.
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    // detect all files inside test folder (add tsx and ts files if the project growth)
+    include: ["**/test/*.{js,tsx,ts}"],
+  },
+});
