b74c02190075e31cdcf5fc752b71a49a98984502
Marc Terradas <terradas.marc@gmail.com>
2023-06-18 17:02:25 +0200

    feat: get discounts

---
+++ infrastructure/getDiscounts.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import { Discount } from "../interfaces/Discount";
+const DISCOUNT_PATH = "./infrastructure/discounts.json";
+/**
+ * get discount list
+ * @returns A Promise that resolves to an array of discounts or `false` if an error occurs.
+ */
+export function getDiscounts(): Promise<Discount[] | false> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      DISCOUNT_PATH,
+      "utf8",
+      (error: NodeJS.ErrnoException | null, data: string) => {
+        if (error) {
+          console.error("Error reading file:", error);
+          reject(false);
+        }
+        try {
+          const discounts: Discount[] = JSON.parse(data);
+          resolve(discounts);
+        } catch (error: NodeJS.ErrnoException) {
+          console.error("Error parsing JSON:", error);
+          reject(false);
+        }
+      }
+    );
+  });
+}
---
+++ test/getDiscounts.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { getDiscounts } from "../infrastructure/getDiscounts";
+import { Discount } from "../interfaces/Discount";
+
+test("get discounts", async () => {
+  const discounts: false | Discount[] = await getDiscounts();
+  expect(discounts).toStrictEqual([
+    { type: "2-for-1", itemNumber: 12 },
+    { type: "spend-X-to-save-Y", threshold: 20, discountAmount: 5 },
+    { type: "menu-pack", items: [12, 21, 37], fixedPrice: 14 },
+  ]);
+});
